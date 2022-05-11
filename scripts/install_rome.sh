28aa5643def0f838a3160423b19d638c9fd2d636
Yiming Zhang <yimingz0@uchicago.edu>
2022-05-11 15:15:11 -0500

    should have actually cloned

--- scripts/install_rome.sh
+++ scripts/install_rome.sh
@@ -7,7 +7,7 @@ REPO_DIR=$(dirname $SCRIPTS_DIR)
 cd $REPO_DIR
 
 echo "Cloning rome into ${REPO_DIR}..."
-# git clone -q https://github.com/kmeng01/rome.git
+git clone -q https://github.com/kmeng01/rome.git
 
 echo "installing rome dependencies..."
 ./rome/scripts/setup_conda.sh
